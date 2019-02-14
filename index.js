912b726c4909b4451794406ac5893aa6e5d38008
sora <soralee2@grovesoft.net>
2019-02-14 10:23:36 +0900

    commit!!!

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hi');
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
